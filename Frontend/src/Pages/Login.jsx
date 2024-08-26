5835f0d12271f1c5ab357c9fc8ce64a3ec8f66cc
Arfan Abid <iffionex111@gmail.com>
2024-08-27 00:05:26 +0500

    Login and Register Page UI + updated UI

--- Frontend/src/Pages/Login.jsx
+++ Frontend/src/Pages/Login.jsx
@@ -1,12 +1,76 @@
-import React from 'react'
-
+import React from "react";
+import { Link } from "react-router-dom";
 function Login() {
   return (
     <>
-
+            <div className="">
+        <section className=" ">
+          <div className="mx-auto flex flex-col items-center justify-center px-6 py-8 md:h-screen lg:py-0">
+            <h1 className="text-2xl font-bold  mb-1 text-white-900 md:text-3xl">Login Page</h1>
+            <div className="w-full rounded-lg bg-white shadow sm:max-w-md md:mt-0 xl:p-0 dark:border dark:border-gray-700 dark:bg-gray-800">
+              <div className="space-y-4 p-6 sm:p-8 md:space-y-6">
+                <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                  Verify Credential
+                </h1>
+                <form className="space-y-4 md:space-y-6" action="#">
+                  <div>
+                    <label
+                      for="email"
+                      className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      Your email
+                    </label>
+                    <input
+                      type="email"
+                      name="email"
+                      id="email"
+                      className="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+                      placeholder="name@company.com"
+                      required=""
+                    />
+                  </div>
+                  
+                  <div>
+                    <label
+                      for="password"
+                      className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      Password
+                    </label>
+                    <input
+                      type="password"
+                      name="password"
+                      id="password"
+                      placeholder="••••••••"
+                      className="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+                      required=""
+                    />
+                  </div>
+                  
+        
+                  <button
+                    type="submit"
+                    className="bg-[#48e1b1] hover:bg-[#9df3d7] focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 w-full rounded-lg px-5 py-2.5 text-center text-sm font-medium text-white focus:outline-none focus:ring-4"
+                  >
+                    Create an account
+                  </button>
+                  <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                    Don't have an account?{" "}
+                    <Link
+                      to="/register"
+                      className="text-primary-600 dark:text-primary-500 font-medium hover:underline"
+                    >
+                      Register here
+                    </Link>
+                  </p>
+                </form>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
     </>
-
-  )
+  );
 }
 
-export default Login
+export default Login;
--- Frontend/src/Pages/NotFound.jsx
+++ Frontend/src/Pages/NotFound.jsx
@@ -5,14 +5,12 @@ import { RiArrowGoBackFill } from "react-icons/ri";
 
 export const NotFound = () => {
   return (
-    <div class="bg-gray-100">
       <div class="h-screen flex flex-col justify-center items-center">
-        <h1 class="text-8xl font-bold text-gray-800">404</h1>
-        <p class="text-4xl font-medium text-gray-800">Page Not Found 🌐</p>
-        <Link to="/" class="mt-4 text-xl text-blue-600 hover:underline">
+        <h1 class="text-2xl md:text-8xl font-bold dark:text-gray-300">404 </h1>
+        <p class="text-xl md:text-4xl font-medium dark:text-gray-400">Page Not Found 🌐</p>
+        <Link to="/" class="mt-4 text-xl text-[#48e1b1]  hover:underline">
           Go back home
         </Link>
       </div>
-    </div>
   );
 };
