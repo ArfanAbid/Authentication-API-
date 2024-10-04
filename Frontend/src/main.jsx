f27f3420bda37271966add0cee3c356ba1d8540f
Arfan Abid <iffionex111@gmail.com>
2024-10-04 10:54:26 +0500

    Deployment Issue Working v3

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { ThemeProvider } from "@/components/theme-provider"
+// import { ThemeProvider } from "@/components/theme-provider"
 import { store } from "./store/store.js";
 import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>  
     <Provider store={store}>
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    {/* <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme"> */}
       <App />
-    </ThemeProvider>
+    {/* </ThemeProvider> */}
     </Provider>
   </React.StrictMode>
 );
